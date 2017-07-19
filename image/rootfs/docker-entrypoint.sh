6ab94adb6559601f69fb3b440930dd68c899ea92
Erwin Müller <erwin.mueller@deventm.org>
2017-07-19 14:31:21 +0200

    Gitolite with unprivileged sshd

---
+++ image/rootfs/docker-entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -xe
+
+function check_files_exists() {
+  ls $1 1> /dev/null 2>&1
+}
+
+function copy_file() {
+  file="$1"; shift
+  dir="$1"; shift
+  mod="$1"; shift
+  if [ -e "$file" ]; then
+    cp "$file" $dir/"$file"
+    chmod $mod $dir/"$file"
+  fi
+}
+
+function copy_sshd() {
+  dir="/ssh-in"
+  if [ ! -d "${dir}" ]; then
+    return
+  fi
+  cd "${dir}"
+  if check_files_exists "ssh_host_*"; then
+    rsync -u -v ssh_host_* /etc/ssh/
+  fi
+  if check_files_exists "sshd_config"; then
+    rsync -u -v sshd_config /etc/ssh/
+  fi
+}
+
+function patch_sshd() {
+  sed -i -e "s/UsePrivilegeSeparation yes/UsePrivilegeSeparation no/" /etc/ssh/sshd_config
+  sed -i -e "s/Port 22/Port ${SSH_PORT}/" /etc/ssh/sshd_config
+}
+
+echo "Running as `id`"
+copy_sshd
+patch_sshd
+cd "${GIT_ROOT}"
+exec /init.sh "$@"
